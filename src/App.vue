8d7fbcc116280bdaaa7fd72a843026855ba9c70e
Tom Pillokat <tom.pillokat@ambient.digital>
2024-06-01 18:30:32 +0200

    add first stupid looking UI

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
-<script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isVegan = ref(false);
+
+const getCoordinates = () => new Promise<GeolocationPosition>((resolve, reject) => {
+  navigator.geolocation.getCurrentPosition(resolve, reject);
+});
+
+const onSearch = async () => {
+  const coordinates = await getCoordinates();
+
+  const data = {
+    isVegan: isVegan.value,
+    latitude: coordinates.coords.latitude,
+    longitude: coordinates.coords.longitude,
+  };
+
+  return data
+};
 </script>
 
 <template>
-  welcome to wot2eat
+  <div class="container" :class="isVegan ? 'vegan-background' : 'non-vegan-background'">
+    <label class="vegan-input">
+      Vegan
+      <input v-model="isVegan" type="checkbox"></input>
+    </label>
+
+    <button @click="onSearch">Search</button>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.vegan-background {
+  background-color: lightgreen;
+}
+
+.non-vegan-background {
+  background-color: lightcoral;
+}
+
+.vegan-input {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 </style>
